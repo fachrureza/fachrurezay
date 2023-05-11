a7b8c19da11149b5c52f45089464f12683a0d259
fachrurezayubesta <fachrurezayubesta20@gmail.com>
2023-05-11 13:55:51 +0700

    first commit

---
+++ js/style.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n){
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n){
+    var i;
+    var imgList = document.getElementsByClassName("mySlides");
+    if (n > imgList.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = imgList.length};
+    for (i = 0; i < imgList.length; i++){
+        imgList[i].style.display = "none";
+    }
+    imgList[i].style.display = "block";
+}
+
+function nameValidation(name, email){
+    if (name != ""){
+        alert("berhasil")
+
+    }else{
+        alert("kosong")
+    }
+}
